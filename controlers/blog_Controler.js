b548ec30399a16480ea73692246486c4a23a817a
Masum Kazi <masum.kazi.hantmk@gmail.com>
2022-11-26 20:14:30 +0600

    image upload and show

--- controlers/blog_Controler.js
+++ controlers/blog_Controler.js
@@ -6,11 +6,11 @@ module.exports = {
 
     index: (req, res, next) => {
         // blog list
-        BlogModel.find((err,docs)=>{
-            if(err){
-                return res.json({error:"Something went wrong!"+err})
+        BlogModel.find((err, docs) => {
+            if (err) {
+                return res.json({ error: "Something went wrong!" + err })
             }
-            return res.json({blogs:docs});
+            return res.json({ blogs: docs });
         })
         res.render('backend/blog/index', { title: 'Blogs', layout: 'backend/layout' });
     },
@@ -29,13 +29,13 @@ module.exports = {
 
     //Blog Show
     show: (req, res, next) => {
-        // BlogModel.find((err,docs)=>{
-        //     if(err){
-        //         return res.json({error:"Something went wrong!"+err})
-        //     }
-        //     return res.json({blogs:docs});
-        // })
-        // res.render('index', { title: 'Blog show', layout: 'backend/layout' }),
+        BlogModel.findById(req.params.id)
+            .then((blog) => {
+                res.json({ "blog": blog });
+            })
+            .catch((err) => {
+                res.json({ "error": "Something went wrong" });
+            })
     },
     //Blog Store
     store: (req, res, next) => {
@@ -45,44 +45,44 @@ module.exports = {
             return res.json({ errors: errors.mapped() });
         }
 
-        // let sampleFile;
-        // if (!req.files || object.keys(req.files).length === 0) {
-        //     return res.status(400).send('No files were uploaded.');
-        // }
+        let sampleFile;
+        if (!req.files || object.keys(req.files).length === 0) {
+            return res.status(400).send('No files were uploaded.');
+        }
 
-        // // The name of the input field (i.e. "sampleFile") is used to retrive the uploaded file
-        // sampleFile = req.files.image;
-        // let rnd = new Date().valueOf();
-        // let filePath = 'upload/' +rnd+sampleFile.name;
+        // The name of the input field (i.e. "sampleFile") is used to retrive the uploaded file
+        sampleFile = req.files.image;
+        let rnd=new Date().valueOf();
+        let filePath = 'upload/' +rnd+sampleFile.name;
 
-        // // Use the mv() method to place the file somewhere on your server
+        // Use the mv() method to place the file somewhere on your server
 
-        // sampleFile.mv('public/'+filePath, function(err) {
-        //     if (err)
-        //     return res.status(500).send(err);
+        sampleFile.mv('public/' + filePath, function (err) {
+            if (err)
+                return res.status(500).send(err);
 
-        //     res.send('File Uploaded!');
-        // });
-    
+            res.send('File Uploaded!');
+        });
+
 
         // Send data to Database
         const blog = new BlogModel({
-        title: req.body.title,
-        slug: req.body.slug,
-        details: req.body.details,
-        image: req.body.image,
-    });
+            title: req.body.title,
+            slug: req.body.slug,
+            details: req.body.details,
+            image: filePath
+        });
 
-    blog.save((err, newBlog) => {
-        if (err) {
-            return res.json({ error: "Something went wrong!" + err })
-        }
-        return res.json({ blog: newBlog });
-    });
+        blog.save((err, newBlog) => {
+            if (err) {
+                return res.json({ error: "Something went wrong!" + err })
+            }
+            return res.json({ blog: newBlog });
+        });
 
-    // return res.json(req.body);
-    // res.render('backend/blog/store', { title: 'Blog Store', layout: 'backend/layout' });
-},
+        // return res.json(req.body);
+        // res.render('backend/blog/store', { title: 'Blog Store', layout: 'backend/layout' });
+    },
 
     //Blog Update
     update: (req, res, next) =>
