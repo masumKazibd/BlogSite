7c57984e4450f0421088347209eb5608cf557a35
Masum Kazi <masum.kazi.hantmk@gmail.com>
2022-12-13 12:58:26 +0600

    Testimonial Showing on frontend

--- controlers/frontend_Controler.js
+++ controlers/frontend_Controler.js
@@ -1,10 +1,12 @@
 const config = require('../config/index');
 const TeamModel = require('../models/team');
 const BlogModel = require('../models/blog');
+const TestimonialModel = require('../models/testimonial');
+const AboutModel = require('../models/about');
 
 module.exports={
     home: (req, res, next) =>  {
-        // blog list
+        // Testimonial list
         BlogModel.find((err, docs) => {
             if (err) {
                 return res.json({ error: "Something went wrong!" + err })
@@ -69,6 +71,24 @@ module.exports={
     about: (req, res, next) =>
         res.render('frontend/about', { title: 'About Us' }),
     
-    testimonial: (req, res, next) =>
-        res.render('frontend/testimonial', { title: 'About Us' }),
+    testimonial: (req, res, next) =>{
+        TestimonialModel.find((err, docs) => {
+            if (err) {
+                return res.json({ error: "Something Went Wrong" + err })
+            }
+            // return res.json({ testimonials: docs });
+
+            const testimonials = [];
+            docs.forEach(Element => {
+                testimonials.push({
+                    title: Element.title,
+                    designation: Element.designation,
+                    details: Element.details,
+                    id: Element._id,
+                    image: Element.image
+                });
+            });
+            res.render('frontend/testimonial', { title: 'List of testimonial', testimonial: testimonials })
+        });
     }
+}
